bf7feb9d66ed205256b336cec1cd17ff7bafb722
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-12 21:39:18 -0500

    Evento click para categoria activa de las mascotas

--- js/index.js
+++ js/index.js
@@ -3,59 +3,52 @@ import { pets } from "./datos.js";
 const categories = document.querySelectorAll(".pet-category");
 
 categories.forEach(element => {
-    element.addEventListener
-
-    
+    element.addEventListener("click", () => {
+        categories.forEach(category =>category.classList.remove("active-category"));
+        element.classList.add("active-category");
+    })
 });
-dogsCategoryBtn.addEventListener("click", () => {
-    dogsCategoryBtn.classList.remove("inactive-category");
-    catsCategoryBtn.classList.add("inactive-category");
-})
 
-catsCategoryBtn.addEventListener("click", () => {
-    catsCategoryBtn.classList.remove("inactive-category");
-    dogsCategoryBtn.classList.add("inactive-category");
-})
 
-const cardsContainer = document.querySelector(".pets-cards-container");
+// const cardsContainer = document.querySelector(".pets-cards-container");
 
-dogsCategoryBtn.addEventListener("click", () => {
-    cardsContainer.innerHTML = "";
-    pets.forEach(element => {
-        const { category, id, name, img, breed } = element;
-        if (category === "dog") {
-            const cardAnchor = document.createElement("a");
-            cardAnchor.href = `detalle-mascota.html?id=${id}`;
-            cardAnchor.innerHTML = `<div class="img-card-container">
-                <img src=${img} alt="Imagen de la mascota" class="img-fluid" />
-                <div class="gradient"></div>
-                <div class="cards-text-container">
-                    <h3 class="Pet-name">${name}</h3>
-                    <p class="pet-breed">${breed}</p>
-                </div>
-            </div>`;
-            cardsContainer.appendChild(cardAnchor);
-        }
-    });
-})
+// dogsCategoryBtn.addEventListener("click", () => {
+//     cardsContainer.innerHTML = "";
+//     pets.forEach(element => {
+//         const { category, id, name, img, breed } = element;
+//         if (category === "dog") {
+//             const cardAnchor = document.createElement("a");
+//             cardAnchor.href = `detalle-mascota.html?id=${id}`;
+//             cardAnchor.innerHTML = `<div class="img-card-container">
+//                 <img src=${img} alt="Imagen de la mascota" class="img-fluid" />
+//                 <div class="gradient"></div>
+//                 <div class="cards-text-container">
+//                     <h3 class="Pet-name">${name}</h3>
+//                     <p class="pet-breed">${breed}</p>
+//                 </div>
+//             </div>`;
+//             cardsContainer.appendChild(cardAnchor);
+//         }
+//     });
+// })
 
-catsCategoryBtn.addEventListener("click", () => {
-    cardsContainer.innerHTML = "";
-    pets.forEach(element => {
-        if (element.category === "cat") {
-            const cardAnchor = document.createElement("a");
-            cardAnchor.href = "detalle-mascota.html";
-            cardAnchor.innerHTML = `<a href="detalle-mascota.html">
-            <div class="img-card-container">
-                <img src=${element.img} alt="Imagen de la mascota" class="img-fluid" />
-                <div class="gradient"></div>
-                <div class="cards-text-container">
-                    <h3 class="Pet-name">${element.name}</h3>
-                    <p class="pet-breed">${element.breed}</p>
-                </div>
-            </div>
-        </a>`;
-            cardsContainer.appendChild(cardAnchor);
-        }
-    });
-})
+// catsCategoryBtn.addEventListener("click", () => {
+//     cardsContainer.innerHTML = "";
+//     pets.forEach(element => {
+//         if (element.category === "cat") {
+//             const cardAnchor = document.createElement("a");
+//             cardAnchor.href = "detalle-mascota.html";
+//             cardAnchor.innerHTML = `<a href="detalle-mascota.html">
+//             <div class="img-card-container">
+//                 <img src=${element.img} alt="Imagen de la mascota" class="img-fluid" />
+//                 <div class="gradient"></div>
+//                 <div class="cards-text-container">
+//                     <h3 class="Pet-name">${element.name}</h3>
+//                     <p class="pet-breed">${element.breed}</p>
+//                 </div>
+//             </div>
+//         </a>`;
+//             cardsContainer.appendChild(cardAnchor);
+//         }
+//     });
+// })
