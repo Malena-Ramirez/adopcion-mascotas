2ea572f6927c9fdeb135194667be2bc014237bef
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-12 22:42:21 -0500

    Correccion de evento click para las cards

--- js/index.js
+++ js/index.js
@@ -3,52 +3,27 @@ import { pets } from "./datos.js";
 const categories = document.querySelectorAll(".pet-category");
 
 categories.forEach(element => {
-    element.addEventListener("click", () => {
-        categories.forEach(category =>category.classList.remove("active-category"));
+    element.addEventListener("click", e => {
+        categories.forEach(category => category.classList.remove("active-category"));
         element.classList.add("active-category");
+        const { category: selectedCategory } = e.target.closest("li").dataset;
+        const cardsContainer = document.querySelector(".pets-cards-container");
+        cardsContainer.innerHTML = "";
+        pets.forEach(element => {
+            const { category, id, name, img, breed } = element;
+            if (selectedCategory === category) {
+                const cardAnchor = document.createElement("a");
+                cardAnchor.href = `detalle-mascota.html?id=${id}`;
+                cardAnchor.innerHTML = `<div class="img-card-container">
+                <img src=${img} alt="Imagen de la mascota" class="img-fluid" />
+                <div class="gradient"></div>
+                <div class="cards-text-container">
+                    <h3 class="Pet-name">${name}</h3>
+                    <p class="pet-breed">${breed}</p>
+                </div>
+            </div>`;
+                cardsContainer.appendChild(cardAnchor);
+            }
+        });
     })
 });
-
-
-// const cardsContainer = document.querySelector(".pets-cards-container");
-
-// dogsCategoryBtn.addEventListener("click", () => {
-//     cardsContainer.innerHTML = "";
-//     pets.forEach(element => {
-//         const { category, id, name, img, breed } = element;
-//         if (category === "dog") {
-//             const cardAnchor = document.createElement("a");
-//             cardAnchor.href = `detalle-mascota.html?id=${id}`;
-//             cardAnchor.innerHTML = `<div class="img-card-container">
-//                 <img src=${img} alt="Imagen de la mascota" class="img-fluid" />
-//                 <div class="gradient"></div>
-//                 <div class="cards-text-container">
-//                     <h3 class="Pet-name">${name}</h3>
-//                     <p class="pet-breed">${breed}</p>
-//                 </div>
-//             </div>`;
-//             cardsContainer.appendChild(cardAnchor);
-//         }
-//     });
-// })
-
-// catsCategoryBtn.addEventListener("click", () => {
-//     cardsContainer.innerHTML = "";
-//     pets.forEach(element => {
-//         if (element.category === "cat") {
-//             const cardAnchor = document.createElement("a");
-//             cardAnchor.href = "detalle-mascota.html";
-//             cardAnchor.innerHTML = `<a href="detalle-mascota.html">
-//             <div class="img-card-container">
-//                 <img src=${element.img} alt="Imagen de la mascota" class="img-fluid" />
-//                 <div class="gradient"></div>
-//                 <div class="cards-text-container">
-//                     <h3 class="Pet-name">${element.name}</h3>
-//                     <p class="pet-breed">${element.breed}</p>
-//                 </div>
-//             </div>
-//         </a>`;
-//             cardsContainer.appendChild(cardAnchor);
-//         }
-//     });
-// })
