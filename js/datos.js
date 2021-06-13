a0e9b42497346b0f439532075462df0ac646e832
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-12 22:46:15 -0500

    Cambio de stories por story

--- js/datos.js
+++ js/datos.js
@@ -37,16 +37,16 @@ const stories = {
 
 const pets = [
     new Pet({ id: 1, name: "Toby", img: "./img/toby.jpg", sex: "male", category: "dog", breed: "Golden retriever", age: "1 año", personality: [personality.c, personality.t, personality.i], story: stories.toby, publishedBy: "Pepita Gonzalez" }),
-    new Pet({ id: 2, name: "Princesa", img: "./img/princesa.jpg", sex: "female", category: "dog", breed: "French poodle", age: "10 meses", personality: [personality.t, personality.i, personality.j], stories: stories.princesa, publishedBy: "Juan Perez" }),
-    new Pet({ id: 3, name: "Milo", img: "./img/milo.jpg", sex: "male", category: "dog", breed: "Jack russell", age: "2 años", personality: [personality.c, personality.t, personality.j], stories: stories.milo, publishedBy: "Luisa Salas" }),
-    new Pet({ id: 4, name: "Luna", img: "./img/luna.jpg", sex: "female", category: "dog", breed: "Yorkshire terrier", age: "3 meses", personality: [personality.c, personality.t, personality.i], stories: stories.luna, publishedBy: "Andrea Díaz" }),
-    new Pet({ id: 5, name: "Coco", img: "./img/coco.jpg", sex: "male", category: "dog", breed: "Border collie", age: "3 meses", personality: [personality.c, personality.t, personality.i], stories: stories.coco, publishedBy: "Andres Rodriguez" }),
-    new Pet({ id: 6, name: "Teo", img: "./img/teo.jpg", sex: "male", category: "dog", breed: "Labrador", age: "1 año", personality: [personality.c, personality.i, personality.j], stories: stories.teo, publishedBy: "Juana Rodriguez" }),
-    new Pet({ id: 7, name: "Felix", img: "./img/felix.jpg", sex: "male", category: "cat", breed: "American shorthair", age: "11 meses", personality: [personality.t, personality.i, personality.j], stories: stories.felix, publishedBy: "Pepita Gonzalez" }),
-    new Pet({ id: 8, name: "Michi", img: "./img/michi.jpg", sex: "male", category: "cat", breed: "British shorthair", age: "2 años", personality: [personality.c, personality.t, personality.j], stories: stories.michi, publishedBy: "Juan Perez" }),
-    new Pet({ id: 9, name: "Lili", img: "./img/lili.jpg", sex: "female", category: "cat", breed: "Persa", age: "1 año", personality: [personality.c, personality.t, personality.j], stories: stories.lili, publishedBy: "Luisa Salas" }),
-    new Pet({ id: 10, name: "Kity", img: "./img/kity.jpg", sex: "female", category: "cat", breed: "Persa", age: "9 meses", personality: [personality.c, personality.i, personality.j], stories: stories.kity, publishedBy: "Andrea Díaz" }),
-    new Pet({ id: 11, name: "Sasha", img: "./img/sasha.jpg", sex: "female", category: "cat", breed: "Siames", age: "2 meses", personality: [personality.c, personality.t, personality.j], stories: stories.sasha, publishedBy: "Juana Rodriguez" })
+    new Pet({ id: 2, name: "Princesa", img: "./img/princesa.jpg", sex: "female", category: "dog", breed: "French poodle", age: "10 meses", personality: [personality.t, personality.i, personality.j], story: stories.princesa, publishedBy: "Juan Perez" }),
+    new Pet({ id: 3, name: "Milo", img: "./img/milo.jpg", sex: "male", category: "dog", breed: "Jack russell", age: "2 años", personality: [personality.c, personality.t, personality.j], story: stories.milo, publishedBy: "Luisa Salas" }),
+    new Pet({ id: 4, name: "Luna", img: "./img/luna.jpg", sex: "female", category: "dog", breed: "Yorkshire terrier", age: "3 meses", personality: [personality.c, personality.t, personality.i], story: stories.luna, publishedBy: "Andrea Díaz" }),
+    new Pet({ id: 5, name: "Coco", img: "./img/coco.jpg", sex: "male", category: "dog", breed: "Border collie", age: "3 meses", personality: [personality.c, personality.t, personality.i], story: stories.coco, publishedBy: "Andres Rodriguez" }),
+    new Pet({ id: 6, name: "Teo", img: "./img/teo.jpg", sex: "male", category: "dog", breed: "Labrador", age: "1 año", personality: [personality.c, personality.i, personality.j], story: stories.teo, publishedBy: "Juana Rodriguez" }),
+    new Pet({ id: 7, name: "Felix", img: "./img/felix.jpg", sex: "male", category: "cat", breed: "American shorthair", age: "11 meses", personality: [personality.t, personality.i, personality.j], story: stories.felix, publishedBy: "Pepita Gonzalez" }),
+    new Pet({ id: 8, name: "Michi", img: "./img/michi.jpg", sex: "male", category: "cat", breed: "British shorthair", age: "2 años", personality: [personality.c, personality.t, personality.j], story: stories.michi, publishedBy: "Juan Perez" }),
+    new Pet({ id: 9, name: "Lili", img: "./img/lili.jpg", sex: "female", category: "cat", breed: "Persa", age: "1 año", personality: [personality.c, personality.t, personality.j], story: stories.lili, publishedBy: "Luisa Salas" }),
+    new Pet({ id: 10, name: "Kity", img: "./img/kity.jpg", sex: "female", category: "cat", breed: "Persa", age: "9 meses", personality: [personality.c, personality.i, personality.j], story: stories.kity, publishedBy: "Andrea Díaz" }),
+    new Pet({ id: 11, name: "Sasha", img: "./img/sasha.jpg", sex: "female", category: "cat", breed: "Siames", age: "2 meses", personality: [personality.c, personality.t, personality.j], story: stories.sasha, publishedBy: "Juana Rodriguez" })
 ];
 
 export { pets };
