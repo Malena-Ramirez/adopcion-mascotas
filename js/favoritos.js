b8aef650a67542fe38453617ee58d439ebd6d453
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-14 00:10:21 -0500

    Llenado de las cards de favoritos desde js

--- js/favoritos.js
+++ js/favoritos.js
@@ -4,7 +4,25 @@ console.log(petsFavArray);
 if (!petsFavArray.length) {
   petsContainer.innerHTML = `
   <p>Aún no has agregado ninguna mascota a favoritos.</p>
-  `
+  `;
 } else {
-  console.log("está lleno")
+  petsContainer.innerHTML = `
+  <div class="pets-cards-container">
+  `;
+  petsFavArray.forEach(element => {
+    const { id, name, img, breed } = element;
+    const cardAnchor = document.createElement("a");
+    cardAnchor.href = `detalle-mascota.html?id=${id}`;
+    cardAnchor.innerHTML = `
+<div class="img-card-container">
+  <img src=${img} alt="Imagen de la mascota" class="img-fluid" />
+  <div class="gradient"></div>
+  <div class="cards-text-container">
+    <h3 class="Pet-name">${name}</h3>
+    <p class="pet-breed">${breed}</p>
+  </div>
+</div>`;
+    document.querySelector(".pets-cards-container").appendChild(cardAnchor);
+  });
+
 }
